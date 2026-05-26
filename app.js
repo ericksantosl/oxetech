27d0ae0c27d5662547f22c8802ee4e0ee3ef7da7
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2026-05-26 19:14:28 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import http from 'https';
+
+const serve = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", 'text/plain');
+    res.end('Hello, World');
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('Servidor rodando!')
+})
